564326ab65ddd925b7cff13ed93192e18e9957a8
keddib <keddib@student.1337.ma>
2021-04-16 13:31:11 +0000

    ftps

--- src/ftps/src/run.sh
+++ src/ftps/src/run.sh
@@ -1,18 +1,14 @@
 rm /etc/vsftpd/vsftpd.conf
 mv /src/vsftpd.conf  /etc/vsftpd/
+
 echo "1337\n1337" | adduser wolfey
-mkdir /home/wolfey/ftp
-chown nobody:nogroup /home/wolfey/ftp
-chmod a-w /home/wolfey/ftp
-mkdir /home/wolfey/ftp/upload
-chown wolfey:wolfey /home/wolfey/ftp/upload
-echo "vsftpd test file" | sudo tee /home/wolfey/ftp/upload/test.txt
+echo "1337\n1337" | adduser keddib
+
+echo "vsftpd test file" | sudo tee /home/wolfey/test.txt
+
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/vsftpd.pem -out /etc/ssl/private/vsftpd.pem
 
-
 rc-status
-
-rc-update add vsftpd default
 
 service vsftpd restart
 
